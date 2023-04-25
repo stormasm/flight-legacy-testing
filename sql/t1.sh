f0f62affb19d356e2b55f5b1669afe3d32cc72c1
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2023-04-24 18:07:27 -0700

    align with SL Pr #47

--- sql/t1.sh
+++ sql/t1.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-flight_sql_client --host 0.0.0.0 --port 3033 --username admin --password password "select 1;"
+flight_sql_client --host 0.0.0.0 --port 50060 --username admin --password password "select 1;"
