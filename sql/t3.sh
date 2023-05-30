9051d61b7748c10f9f172dc4008e1b20dfe268fe
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2023-05-30 13:07:01 -0700

    more insert statement examples

--- sql/t3.sh
+++ sql/t3.sh
@@ -2,3 +2,5 @@
 flight_sql_client --host 0.0.0.0 --port 50060 --username admin --password password "insert into person (id, name) values (1, 'Hopper'), (2, 'Kay')"
 flight_sql_client --host 0.0.0.0 --port 50060 --username admin --password password "insert into human (id, name) values (1, 'Hopper'), (2, 'Kay')"
 flight_sql_client --host 0.0.0.0 --port 50060 --username admin --password password "insert into human values (3, 'Smith'), (4, 'Rick')"
+flight_sql_client --host 0.0.0.0 --port 50060 --username admin --password password "insert into human values (5, 'Jones')"
+flight_sql_client --host 0.0.0.0 --port 50060 --username admin --password password "insert into human values (6, 'Indiana')"
